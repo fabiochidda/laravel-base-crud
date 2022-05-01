a39b4e049fcef6ef41b367cf8a935666712f635e
fabiochidda <fabio.chidda99@gmail.com>
2022-05-01 16:48:21 +0200

    inserita la tabella

--- resources/views/Comics/index.blade.php
+++ resources/views/Comics/index.blade.php
@@ -2,6 +2,48 @@
 
 @section('mainContent')
     
-    <h1>Ciao</h1>
+    <table>
+
+        <thead>
+            <th>Titolo</th>
+            <th>Immagine</th>
+            <th>Descrizione</th>
+            <th>Serie</th>
+            <th>Sale Date</th>
+            <th>Tipo</th>
+            <th>Prezzo</th>
+        </thead>
+
+        <tbody>
+            @foreach ($comics as $el)
+                <tr>
+                    <td>
+                        {{$el->title}}
+                    </td>
+                    <td>
+                        <figure>
+                            <img src="{{$el->thumb}}" alt="">
+                        </figure>
+                    </td>
+                    <td>
+                        {{$el->description}}
+                    </td>
+                    <td>
+                        {{$el->series}}
+                    </td>
+                    <td>
+                        {{$el->sale_date}}
+                    </td>
+                    <td>
+                        {{$el->type}}
+                    </td>
+                    <td>
+                        {{$el->price}}
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+
+    </table>
 
 @endsection
